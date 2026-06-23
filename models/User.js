e8172c87d8a5f08d521724cbe96ebd6dfd82099e
Aditya99977 <Adiofficial8989gmail.com>
2026-06-23 12:46:26 +0530

    Day 1 - Project setup and MongoDB Atlas connection

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+
+    password:{
+        type:String,
+        required:true
+    },
+
+    examTarget:{
+        type:String,
+        default:"IBPS"
+    },
+
+    score:{
+        type:Number,
+        default:0
+    },
+
+    testsAttempted:{
+        type:Number,
+        default:0
+    },
+
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
+});
+
+module.exports = mongoose.model("User", UserSchema);
---
+++ server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/auth", authRoutes);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ MongoDB Connected Successfully");
+  })
+  .catch((err) => {
+    console.log("❌ MongoDB Connection Error");
+    console.log(err.message);
+  });
+
+app.get("/", (req, res) => {
+  res.send("🚀 ExamBooster Backend Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
